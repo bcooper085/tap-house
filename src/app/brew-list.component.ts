194e75059e76c117d784f5f072cac37ccf8f611b
brandon cooper and sandro mijatovic <brandon+sandro@>
2017-03-29 16:36:53 -0700

    more progress

--- src/app/brew-list.component.ts
+++ src/app/brew-list.component.ts
@@ -4,7 +4,7 @@ import { Brew } from './brew.model';
 @Component({
   selector: 'brew-list',
   template: `
-  <select>
+  <select (change)="onChange($event.target.value)">
       <option value=1 selected="selected">One pint</option>
       <option value=2>Two pints</option>
       <option value=3>3 pints</option>
@@ -14,10 +14,13 @@ import { Brew } from './brew.model';
   </ul>
   `
 })
-s
+
 export class BrewListComponent {
+
   @Input() childBrewList: Brew[];
   @Output() clickSender = new EventEmitter();
+
+  removePint: number = "1";
 
   editButtonClicked(brewToEdit: Brew) {
     this.clickSender.emit(brewToEdit);
